a74f7b7bc7a1106effa8399cc0b2cbf49ff05508
Roshan  J <76832165+dev-enforced@users.noreply.github.com>
2021-07-10 23:55:13 +0000

    Lesson4 of Level0 completed

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 // var userName=prompt("Please enter userName:")
 // alert("Hello "+userName);
 
-var btnTranslate=document.querySelector("#btn-translate");
-var txtInput=document.querySelector("#txt-input");
-var outputDiv=document.querySelector("#output");
-function clickEventHandler(){
-  console.log(txtInput.value); //Exercise 4 lesson: .value attribute of input elements
-  outputDiv.innerText=txtInput.value;//Exercise 5 lesson:Wiring the output div part
-}
-
-btnTranslate.addEventListener("click",clickEventHandler);
 
 // Ex7 answer
 var url2="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=I am Iron Man"
@@ -37,3 +28,34 @@ fetch(constructURL(text3))
 })
 
 //functions can be reduced by using arrow representation
+
+// Exercise 9
+var btnTranslate=document.querySelector("#btn-translate");
+var txtInput=document.querySelector("#txt-input");
+var outputDiv=document.querySelector("#output");
+
+var serverURL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var translator="https://api.funtranslations.com/translate/minion.json";
+function constructingURL(text){
+  return translator+"?text="+text;
+}
+
+function errorHandler(error){
+  console.log("Error occured: "+error);
+}
+
+function clickEventHandler(){
+  //console.log(txtInput.value); //Exercise 4 lesson: .value attribute of input elements
+  //outputDiv.innerText=txtInput.value;//Exercise 5 lesson:Wiring the output div part
+  var inp=txtInput.value
+  fetch(constructingURL(inp))
+  .then(response=>response.json())
+  .then(json=>{
+    var op=json.contents.translated;
+    outputDiv.innerText=op;
+  })
+  .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click",clickEventHandler);
